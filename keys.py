994df077ad955278cf6c2246bd4f05d99aad262b
RaulLessa11 <45500735+RaulLessa11@users.noreply.github.com>
2020-07-08 18:13:43 -0300

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,8 @@
+# keys_format.py shares the same format as keys.py.
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# keys there.
+CONSUMER_KEY = 'f5pKiNTfdVE6jMSGwoWZkcUgY'
+CONSUMER_SECRET = '52OmINYT2qbvTA3VpA0TNsd2Ur2spmBo0P5p4R1MQBfRjRezAx'
+ACCESS_KEY = '1279198060653039617-pz3jFLvb4quJwRRm5igws12wiZsl2f'
+ACCESS_SECRET = 'P5eFWbOlhq4ZR6iYmTqfaGDvQ4VoZcSLF26eyJlHvBsPu'	
---
+++ my_twitter_bot.py
@@ -0,0 +1,58 @@
+import tweepy
+import time
+import random
+# NOTE: I put my keys in the keys.py to separate them
+# from this main file.
+# Please refer to keys_format.py to see the format.
+from keys import *
+
+# NOTE: flush=True is just for running this script
+# with PythonAnywhere's always-on task.
+# More info: https://help.pythonanywhere.com/pages/AlwaysOnTasks/
+print('this is my twitter bot', flush=True)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+FILE_NAME = 'last_seen_id.txt'
+        
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...', flush=True)
+    # DEV NOTE: use 1060651988453654528 for testing.
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    # NOTE: We need to use tweet_mode='extended' below to show
+    # all full tweets (with full_text). Without it, long tweets
+    # would be cut off.
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        print('responding back...', flush=True)
+        #Random emoji select an item from a list
+        emoji_list = ['ʕっ•ᴥ•ʔっ','(˘･_･˘)','(ب_ب)','ʕ ꈍᴥꈍʔ','⊂(•‿•⊂ )','( ꈍᴗꈍ)','( ˘ ³˘)', 'っ˘з(˘⌣˘ )']
+        emoji_item = random.choice(emoji_list)
+    
+        #Random number
+        number = random.randint(0, 100)
+        api.update_status('@' + mention.user.screen_name + "  " + "Você tem " + str(number) + "% de chances " + emoji_item, mention.id,in_reply_to_status_id = mention.id )
+    
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
